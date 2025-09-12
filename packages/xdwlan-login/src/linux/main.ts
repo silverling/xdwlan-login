f7d2cb2d2d3e310033e87d74e854d509cdf50da7
Silver Ling <silver.ling@outlook.com>
2025-09-12 20:20:05 +0800

    fix: load config after parsing args

--- packages/xdwlan-login/src/linux/main.ts
+++ packages/xdwlan-login/src/linux/main.ts
@@ -1,18 +1,18 @@
 import { logger } from "@lib/logger";
 import { parseArgs } from "./cli";
-import { daemon, oneshot } from "@lib/mode";
 import { exit, handleSignal } from "@lib/instrumentation";
 
 async function main() {
   handleSignal();
-
   const options = parseArgs();
 
   if (options.oneshot) {
+    const oneshot = await import("@lib/mode").then((m) => m.oneshot);
     logger.info("Start. Running in oneshot mode.");
     await oneshot();
     exit();
   } else {
+    const daemon = await import("@lib/mode").then((m) => m.daemon);
     logger.info("Start. Running in daemon mode.");
     await daemon();
   }
